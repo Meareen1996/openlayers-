6be114da24b908ab355e2fcd4f0c410bba746f0c
yang <sjmhzxz1996@gmail.com>
2021-09-08 18:57:43 +0800

    fix

--- src/components/modules/GeoServer.vue
+++ src/components/modules/GeoServer.vue
@@ -1,64 +1,60 @@
-<!--
- * @Author: liuzhenghe
- * @Email: 15901450207@163.com
- * @Date: 2020-08-04 11:43:41
- * @LastEditors: liuzhenghe
- * @LastEditTime: 2020-09-21 11:10:43
- * @Descripttion: 加载 GeoServer 发布的 wms 服务
--->
+<!-- 加载 GeoServer 发布的 wms 服务-->
 
 <template>
-  <div id="map-container"
-       style="width:100%;height:100vh;">
-  </div>
+  <div id="map-container" style="width: 100%; height: 100vh"></div>
 </template>
 <script>
-import 'ol/ol.css'
-import Map from 'ol/Map'
-import View from 'ol/View'
-import TileLayer from 'ol/layer/Tile'
-import TileWMS from "ol/source/TileWMS"
-import OSM from 'ol/source/OSM'
-import { defaults as defaultControls } from 'ol/control'
-import ZoomSlider from 'ol/control/ZoomSlider'
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import TileWMS from "ol/source/TileWMS";
+import OSM from "ol/source/OSM";
+import { defaults as defaultControls } from "ol/control";
+import ZoomSlider from "ol/control/ZoomSlider";
 
 export default {
-  name: 'GeoServer',
+  name: "GeoServer",
   data() {
     return {
       map: null,
-    }
+    };
   },
   mounted() {
-    this.initMap()
+    this.initMap();
   },
   methods: {
     /**
      * @name: 地图单击事件
      */
     singleClickFun() {
-      this.map.on('singleclick', event => {
-        console.log(event)
+      this.map.on("singleclick", (event) => {
+        console.log(event);
         // GetFeatureInfo
-        let view = this.map.getView()
+        let view = this.map.getView();
         let wmsSource = new TileWMS({
           url: "https://localhost:6443/arcgis/services/xiaoshuidianooooo/MapServer/WMSServer",
           // params: { 'LAYERS': 'topp:states', 'TILED': true },
           // serverType: "geoserver",
           // transition: 0,
-        })
-        let viewResolution = (view.getResolution())
+        });
+        let viewResolution = view.getResolution();
         let url = wmsSource.getFeatureInfoUrl(
-          event.coordinate, viewResolution, 'EPSG:4326',
-          { 'INFO_FORMAT': 'application/json' })
+          event.coordinate,
+          viewResolution,
+          "EPSG:4326",
+          { INFO_FORMAT: "application/json" }
+        );
         if (url) {
-          fetch(url).then(response => {
-            return response.json()
-          }).then(data => {
-            console.log(data)
-          })
+          fetch(url)
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              console.log(data);
+            });
         }
-      })
+      });
     },
 
     /**
@@ -66,40 +62,40 @@ export default {
      */
     initMap() {
       let view = new View({
-        projection: 'EPSG:4326',
+        projection: "EPSG:4326",
         center: [-101.44058, 39.816105],
         zoom: 5,
-      })
+      });
       let layer = new TileLayer({
         source: new OSM(),
         visible: true,
         zIndex: 1,
-        name: 'OSM',
-      })
+        name: "OSM",
+      });
       this.map = new Map({
         layers: [],
-        target: 'map-container',
+        target: "map-container",
         view: view,
         controls: defaultControls().extend([new ZoomSlider()]),
-      })
-      this.map.addLayer(layer)
+      });
+      this.map.addLayer(layer);
 
       // 加载 GeoServer 发布的 wms 服务
       let wmsLayer = new TileLayer({
         source: new TileWMS({
-          url: 'https://localhost:6443/arcgis/rest/services/xiaoshuidian/MapServer/wms', // geoserver 服务地址
-          params: { 'LAYERS': 'topp:states', 'TILED': true },
+          url: "https://localhost:6443/arcgis/rest/services/xiaoshuidian/MapServer/wms", // geoserver 服务地址
+          params: { LAYERS: "topp:states", TILED: true },
           serverType: "geoserver",
           transition: 0,
         }),
         visible: true,
         zIndex: 9,
-        className: 'wms'
-      })
-      this.map.addLayer(wmsLayer)
+        className: "wms",
+      });
+      this.map.addLayer(wmsLayer);
 
-      this.singleClickFun()
+      this.singleClickFun();
     },
   },
-}
+};
 </script>
